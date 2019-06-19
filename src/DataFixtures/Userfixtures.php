575c153b4fa96195c498bc9408d35d7deefcbfa7
rodmedeiros <rodsmedeiros@gmail.com>
2019-06-19 17:37:34 -0300

    implementing basic login

--- src/DataFixtures/Userfixtures.php
+++ src/DataFixtures/Userfixtures.php
@@ -4,14 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class Userfixtures extends Fixture
 {
+    private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        // ...
 
-        $manager->flush();
+        $user->setPassword($this->passwordEncoder->encodePassword(
+             $user,
+             'the_new_password'
+        ));
+
     }
+
+
 }
